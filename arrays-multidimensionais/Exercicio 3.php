efc993e62e1a444c10c7ca8d7bcdc97371ed7215
Soulokaocom <lucasazevedoabrittaguidine@gmail.com>
2020-12-05 23:34:43 -0300

    Exercício 3

---
+++ arrays-multidimensionais/Exercicio 3.php
@@ -0,0 +1,5 @@
+<?php
+    list ($musica1, $musica2, $musica3, $musica4, $musica5) = array ("Casanapria - Recayd Mob", "Anos Luz - Matuê", "Laughing on the Outside - Bernadette Carroll", "Thunder - Imagine Dragons", "Warriors - Imagine Dragons");
+
+    echo "$musica2<br>$musica4<br>";
+?>
