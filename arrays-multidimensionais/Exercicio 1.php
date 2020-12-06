08afb4af9676b0661547b2b2e2578e5e44f84c7e
Soulokaocom <lucasazevedoabrittaguidine@gmail.com>
2020-12-05 23:52:36 -0300

    Update Exercicio 1.php

--- arrays-multidimensionais/Exercicio 1.php
+++ arrays-multidimensionais/Exercicio 1.php
@@ -1,8 +1,8 @@
-/**Faça uma função que receba como entrada 5
-números e imprima o maior e o menor número
-(dado que todos os números são diferentes).
-Obs.: Use arrays.*/
 <?php
+    //Faça uma função que receba como entrada 5
+    //números e imprima o maior e o menor número
+    //(dado que todos os números são diferentes).
+    //Obs.: Use arrays.
     $matriz = array ( array(1,2,3), array(4,5,6), array (7,8,9) );
         
     foreach ($matriz as $i => $array){
